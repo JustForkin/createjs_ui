3e2e9182c226cbbb47d22fd24d3a9ea36898a889
Andreas Bresser <a.bresser@greyrook.com>
2015-02-01 18:07:37 +0100

    scrollbar, get alignment from ScrollArea content (WIP)

--- example/js/ScrollBarExample.js
+++ example/js/ScrollBarExample.js
@@ -7,37 +7,26 @@ var ScrollBarExample;
 
         // initialize theme
         new createjs_ui.AeonTheme();
+
+        var inner = new createjs_ui.LayoutGroup();
+        inner.layout = new createjs_ui.HorizontalLayout();
+        inner.layout.gap = 10;
         
-        // outer group that hosts some other groups
-        var outer = new createjs_ui.LayoutGroup();
-
-        //outer.width = stage.canvas.width - (grp.x*2);
-        outer.layout = new createjs_ui.VerticalLayout();
-        outer.layout.gap = 10;
-
-        var outer_scroll = new createjs_ui.ScrollArea(outer);
-        outer_scroll.height = 300;
-        outer_scroll.width = 500;
-        outer_scroll.x = 20;
-        outer_scroll.y = 20;
-        this.addChild(outer_scroll);
-
-        for (var j = 0; j < 20; j++) {
-            var inner = new createjs_ui.LayoutGroup();
-            inner.layout = new createjs_ui.HorizontalLayout();
-            inner.layout.gap = 10;
-            // add some buttons
-            for (var i = 0; i < 10; i++) {
-                btn = new createjs_ui.Button();
-                btn.height = 25;
-                btn.label = (j+1) + ". - " + (i+1)+". button";
-                inner.addChild(btn);
-            }
-            var inner_scroll = new createjs_ui.ScrollArea();
-            inner_scroll.content = inner;
-            inner_scroll.width = 500;
-            outer.addChild(inner_scroll);
+        // add some buttons
+        for (var i = 0; i < 10; i++) {
+            btn = new createjs_ui.Button();
+            btn.height = 25;
+            btn.label = (i+1)+". button";
+            inner.addChild(btn);
         }
+        var inner_scroll = new createjs_ui.ScrollArea();
+        inner_scroll.content = inner;
+        inner_scroll.width = 500;
+        this.addChild(inner_scroll);
+        
+        var sb = new createjs_ui.ScrollBar(inner_scroll);
+        this.addChild(sb);
+        sb.y = inner_scroll.height;
     };
 
     var p = createjs.extend(ScrollBarExample, createjs_ui.Application);
--- src/controls/ScrollBar.js
+++ src/controls/ScrollBar.js
@@ -6,21 +6,43 @@ this.createjs_ui = this.createjs_ui || {};
 
     var ScrollBar = function(theme, scrollArea) {
         this.mode = ScrollBar.DESKTOP_MODE;
-        this.scrollArea = scrollArea;
+        if (this.orientation === undefined) {
+            this.orientation = ScrollBar.HORIZONTAL;
+            if (scrollArea && scrollArea.content &&
+                    scrollArea.content.layout.alignment ==
+                    createjs_ui.LayoutAlignment.VERTICAL_ALIGNMENT) {
+                this.orientation = ScrollBar.VERTICAL;
+            }
+        }
+
         this.skinName = this.skinName || ScrollBar.SKIN_NAME;
         
         this.thumb = new createjs_ui.ScrollThumb(theme);
         this.addChild(this.thumb);
-        // TODO: get track from skin
+        
+        // TODO get track from skin
+        
     };
     
     ScrollBar.SKIN_NAME = "scroll_bar";
     
     ScrollBar.DESKTOP_MODE = "desktop";
     ScrollBar.MOBILE_MODE = "mobile";
+    
+    ScrollBar.HORIZONTAL = "horizontal";
+    ScrollBar.VERTICAL = "vertical";
 
     var p = createjs.extend(ScrollBar, createjs_ui.Control);
-    ScrollBar.SKIN_NAME = "scroll_bar";
+    /**
+     * redraw control for current state from theme
+     */
+    p.redraw = function() {
+        createjs_ui.Control.prototype.redraw.call();
+        if (this.invalidState) {
+            
+        }
+        
+    };
 
     createjs_ui.ScrollBar = createjs.promote(ScrollBar, "Control");
 })();
