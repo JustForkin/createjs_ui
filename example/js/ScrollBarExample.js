e1040befd6e495b2b5dd93961d05d08b0c47604d
Björn <bjoern@friedrichs1.de>
2015-02-02 16:47:54 +0100

    implemented scrollbars

--- example/js/ScrollBarExample.js
+++ example/js/ScrollBarExample.js
@@ -8,25 +8,66 @@ var ScrollBarExample;
         // initialize theme
         new createjs_ui.AeonTheme();
 
-        var inner = new createjs_ui.LayoutGroup();
-        inner.layout = new createjs_ui.HorizontalLayout();
-        inner.layout.gap = 10;
+        /*
+         #################################
+         #######     HORIZONTAL   ########
+         #################################
+         */
+
+        var inner_horizontal = new createjs_ui.LayoutGroup();
+        inner_horizontal.layout = new createjs_ui.HorizontalLayout();
+        inner_horizontal.layout.gap = 10;
         
         // add some buttons
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 100; i++) {
+            btn = new createjs_ui.Button();
+            btn.height = 25;
+            btn.label = (i+1)+". button";
+            inner_horizontal.addChild(btn);
+        }
+        var inner_scroll_horizontal = new createjs_ui.ScrollArea();
+        inner_scroll_horizontal.content = inner_horizontal;
+        inner_scroll_horizontal.width = 500;
+        this.addChild(inner_scroll_horizontal);
+
+        var sb_horizontal = new createjs_ui.ScrollBar(inner_scroll_horizontal);
+        this.addChild(sb_horizontal);
+        sb_horizontal.y = inner_scroll_horizontal.height;
+        sb_horizontal.width = inner_scroll_horizontal.width;
+
+
+
+        /*
+            ##################################
+            ########     VERTICAL    #########
+            ##################################
+         */
+
+        var grp = new createjs_ui.LayoutGroup();
+        var inner = new createjs_ui.LayoutGroup();
+        inner.layout = new createjs_ui.VerticalLayout();
+        inner.layout.gap = 10;
+
+        // add some buttons
+        for (var i = 0; i < 12; i++) {
             btn = new createjs_ui.Button();
             btn.height = 25;
             btn.label = (i+1)+". button";
             inner.addChild(btn);
         }
-        var inner_scroll = new createjs_ui.ScrollArea();
-        inner_scroll.content = inner;
-        inner_scroll.width = 500;
-        this.addChild(inner_scroll);
-        
+        var inner_scroll = new createjs_ui.ScrollArea(inner);
+        inner_scroll.width = 100;
+        inner_scroll.height = 150;
+        grp.addChild(inner_scroll);
+
+
         var sb = new createjs_ui.ScrollBar(inner_scroll);
-        this.addChild(sb);
-        sb.y = inner_scroll.height;
+        grp.addChild(sb);
+        sb.x = inner_scroll.width;
+        sb.height = inner_scroll.height;
+
+        this.addChild(grp);
+        grp.y = 150;
     };
 
     var p = createjs.extend(ScrollBarExample, createjs_ui.Application);
--- src/controls/ScrollThumb.js
+++ src/controls/ScrollThumb.js
@@ -4,14 +4,59 @@ this.createjs_ui = this.createjs_ui || {};
 (function() {
     "use strict";
 
-    var ScrollThumb = function(theme) {
+    var ScrollThumb = function(orientation, theme) {
+        this.orientation = orientation || createjs_ui.ScrollBar.HORIZONTAL;
         this.skinName = this.skinName || ScrollThumb.SKIN_NAME;
-        createjs_ui.Button.call(this, theme)
+        this._validStates = ['horizontal_up', 'vertical_up', 'horizontal_down', 'vertical_down', 'horizontal_hover', 'vertical_hover'];
+        createjs_ui.Button.call(this, theme);
+        this.invalidTrack = true;
     };
 
     var p = createjs.extend(ScrollThumb, createjs_ui.Button);
 
     ScrollThumb.SKIN_NAME = "scroll_thumb";
+
+    p._setCurrentState = function(value) {
+        value = this.orientation + '_' + value;
+
+        createjs_ui.Button.prototype._setCurrentState.call(this, value);
+    };
+
+    p.showTrack = function(skin) {
+        if (!skin) {
+            return false;
+        }
+        this.addChild(skin);
+
+        skin.x = (this.width - skin.getBounds().width )/ 2;
+        skin.y = (this.height - skin.getBounds().height )/ 2;
+
+        return true;
+    };
+
+    p.redraw = function() {
+        createjs_ui.Button.prototype.redraw.call(this);
+        if (this.invalidTrack) {
+            var skin = this.theme.getSkin(this, this.skinName, this.orientation+"_thumb");
+            if (this.showTrack(skin)) {
+                this.invalidTrack = false;
+                if (this._redraw_proxy_track !== undefined) {
+                    this.theme.removeEventListener(
+                        "ui_complete", this._redraw_proxy_track);
+                    this._redraw_proxy_track = undefined;
+                }
+            } else {
+                if (this._redraw_proxy_track === undefined) {
+                    // store callback function so it can easily be removed
+                    // and will not be added twice for this control
+                    this._redraw_proxy_track = createjs.proxy(this.showTrack, this);
+                }
+                this.theme.addEventListener(
+                    "ui_complete", this._redraw_proxy_track);
+            }
+            this.invalidTrack = false;
+        }
+    };
 
     createjs_ui.ScrollThumb = createjs.promote(ScrollThumb, "Button");
 })();
